9a3631c3f5eed96aba7b9b66a1172f954dc7fe98
Trevor Mangrum <mangrumtrevor@gmail.com>
2021-11-01 15:20:32 -0400

    Adds layout component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,17 +1,26 @@
 import React from "react";
+import styles from "./footer.module.scss";
 
 const Footer: React.FC = () => {
-    return (
-    <footer>
-        <section>
-            <div></div>
-            <div></div>
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.wrapper}>
+        <section className={styles.footerSection}>
+          <div>
+            <h2>MannaForce</h2>
+          </div>
+          <div>
+            <h2>Contact</h2>
+          </div>
         </section>
-        <section>
-            <span>Website by: <a href='#'>MangrumTech</a></span>
+        <section className={styles.footerSection}>
+          <span>
+            Website by: <a href="#">MangrumTech</a>
+          </span>
         </section>
+      </div>
     </footer>
-    );
+  );
 };
 
 export default Footer;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,16 @@ import styles from "./header.module.scss";
 const Header: React.FC = () => {
     return (
         <nav className={styles.navigation}>
-            <a className={styles.logo} href='/'><img src='/mannaforce.png' /> </a>
-            <div className={styles.linkCollection}>
-                <a className={styles.navLink} href='/'>Home</a>
-                <a className={styles.navLink} href='/about'>About Us</a>
-                <a className={styles.navLink} href='/contact'>Contact Us</a>
-                <a className={styles.navLink} href='/testimonials'>Testimonials</a>
-                <a className={styles.navLink} href='/blog'>Blog</a>
-                <a className={styles.navActionButton} href='/donate'>Donate</a>
+            <div className={styles.wrapper}>
+                <a className={styles.logo} href='/'><img src='/mannaforce.png' /> </a>
+                <div className={styles.linkCollection}>
+                    <a className={styles.navLink} href='/'>Home</a>
+                    <a className={styles.navLink} href='/about'>About Us</a>
+                    <a className={styles.navLink} href='/contact'>Contact Us</a>
+                    <a className={styles.navLink} href='/testimonials'>Testimonials</a>
+                    <a className={styles.navLink} href='/blog'>Blog</a>
+                    <a className={styles.navActionButton} href='/donate'>Donate</a>
+                </div>
             </div>
         </nav>
     );
