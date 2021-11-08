39f74a737e4481c04b150faf4f77a98615f6b6a9
Trevor Mangrum <25257772+trevormangrum@users.noreply.github.com>
2021-11-08 16:04:50 -0500

    Adds basic components for home page (#3)
    * Implements initial view
    
    * Adds basic components for home page
    
    Co-authored-by: Trevor Mangrum <mangrumtrevor@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import HomeTestimonials from "src/components/HomeTestimonials";
+import InitialView from "src/components/InitialView";
 import Layout from "src/components/Layout";
+import WhoWeAre from "src/components/WhoWeAre";
+import Head from "next/head";
 export default function HomePage() {
   return (
     <Layout>
-        <h1>Hello world!</h1>
+      <Head>
+        <title>The Force That Makes a Difference | Manna Force</title>
+        <meta name="description" content="Manna Force is a ...."/>
+      </Head>
+      <InitialView />
+      <WhoWeAre />
+      <HomeTestimonials />
     </Layout>
   );
 }
