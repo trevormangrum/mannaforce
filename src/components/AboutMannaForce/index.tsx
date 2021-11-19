907379705f19acff1e6e60705e0557ebf4389ec5
Trevor Mangrum <tmangrum@pop-os.localdomain>
2021-11-19 12:34:48 -0500

    Adds missing about us content (minus team members)

--- src/components/AboutMannaForce/index.tsx
+++ src/components/AboutMannaForce/index.tsx
@@ -8,7 +8,9 @@ const AboutMannaForce: React.FC = () => {
             <h2>About Manna Force</h2>
             <span><strong>Manna- </strong>A power; a special, supernatural energy; the key to effective, long-term change</span>
             <span><strong>Force- </strong>The capacity for affecting the mind or behavior. The cause to do work or cause physical change; energy, strength, or active power</span>
-            <p>Manna Force is a ...</p>
+            <p>Manna Force is a Not for Profit movement, committed to bringing the truth of addiction to light. We strive to give families and individuals who suffer from its destruction the answers they need to be restored completely to a productive and fulfilling life. We wish to change the commonly held perceptions of society about the fundamentals of addiction and who the addicts are. Manna Force works to alleviate these perceptions that contribute to failure rather than success.</p>
+            <p>Through alliances with professionals who work in the field of addiction and political advocates who are willing to work tirelessly for change, we put in place the building blocks to ensure positive change will come. Through multiple media campaigns, public speaking, and social networking, these building blocks will be used to better serve our communities, counties, states, and society as a whole. Our endgame is to ultimately eradicate the scourge of addiction that has been destroying our families from within.</p>
+            <p>It is time for lasting change. The current system we have in place does not work. People around the United States are suffering due to the ravages of addiction. There  is a better way, a true way, to win this battle and end this war against addiction. By applying the tried and proven addiction recovery and prevention practices from other countries, we can create the answer we need. Manna Force won't stop until every family that is suffering from addiction has the answer.</p>
         </div>
     </section>
     );
--- src/components/Directives/index.tsx
+++ src/components/Directives/index.tsx
@@ -5,18 +5,19 @@ const Directives: React.FC = () => {
     return (
     <section className={styles.container}>
        <div className={styles.wrapper}>
-           <h2>Our Directives</h2>
-           <div className={styles.cardGrid}>
-               <div className={styles.card}>
-                   <h3>Directive #1</h3>
-               </div>
-               <div className={styles.card}>
-                   <h3>Directive #2</h3>
-               </div>
-               <div className={styles.card}>
-                   <h3>Directive #3</h3>
-               </div>
-           </div>
+           <h2>Our Goals</h2>
+           <ol>
+           <li>
+               <h3 className={styles.directive}>To create a national campaign to educate and change the public's perception of addiction and who addicts are.</h3>
+           </li>
+           <li>
+
+               <h3 className={styles.directive}>To change existing laws and pass laws that will benefit families who are powerless in the grips of addiction.</h3>
+           </li>
+           <li>
+               <h3 className={styles.directive}>To help support existing non-profit recovery programs with a high long term success rate.</h3>
+           </li>
+           </ol>
        </div>
     </section>
     );
